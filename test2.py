d68a1065b4c5c22706678160b9688e70ef2abc47
hamidrizvi <35923822+hamidrizvi@users.noreply.github.com>
2020-09-21 23:49:05 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,8 @@
+# a=int(input("Enter first number"))
+# b=int(input("Enter 2nd number"))
+# c=int(input("Enter 3rd number"))
+# avg=(a+b+c)/3
+# print(f"Average of three numbers : {int(avg)}")
+# Alternatively
+d,e,f=(input("Enter three numbers")).split(" ")
+print(f"Average of three numbers = {(int(d)+int(e)+int(f))/3}")
